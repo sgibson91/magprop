d3ad3be3ca0d6b2f87a6c5455b055e2a7d60d6b1
Sarah Gibson <sgibson@turing.ac.uk>
2019-01-23 17:01:27 +0000

    Add fitting statistics to magnetar library
    Add (reduced) chi-square and MC chain convergence functions to
    magnetar library

---
+++ magnetar/fit_stats.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+def redchisq(ydata, ymod, deg=None, sd=None):
+    """
+Returns the reduced chi-square error statistic for an arbitrary model, chisq/nu,
+where nu is the number of degrees of freedom. If individual standard deviations
+(array sd) are supplied, then the chi-square error statistic is computed as the
+sum of squared errors divided by the standard deviations.
+
+    :param ydata: array containing data
+    :param ymod: array containing model
+    :param deg: float number of free parameter in the model
+    :param sd: array of uncertainties in ydata
+    :return: float number the (reduced) chi-square
+    """
+    # Chi-Square statistic
+    if sd is not None:
+        chisq = np.sum(((ydata - ymod) / sd) ** 2.0)
+
+    else:
+        chisq = np.sum((ydata - ymod) ** 2.0)
+
+    if deg is not None:
+        # Number of degrees of freedom assuming 2 free parameters
+        nu = ydata.size - 1.0 - deg
+
+        return chisq / nu
+
+    else:
+        return chisq
+
+
+def conv(samples, N, Nc, Ns):
+    """
+Function to calculate the R test of convergence of MC chains for multiple
+dimensions. See Daniel Mortlock's notes on Comuptational Statistics from the
+ICIC workshop or Gelman, A. and Rubin, D. B. (1992) for details.
+
+    :param samples: array of shape (Nc, Ns, N) that has already had burn-in
+                    removed
+    :param N: int number of fitting parameters (Npars)
+    :param Nc: int number of chain (Nwalk)
+    :param Ns: int number of MC steps (Nstep)
+    :return: array of convergence ratios for each parameter
+    """
+    # 1) Calculate the mean of each chain for each parameter
+    # 2) Calculate the variance of each chain for each parameter
+    xbarc = np.ndarray((Nc, N))    # (1)
+    sigma2c = np.ndarray((Nc, N))  # (2)
+    for i in range(N):
+        for j in range(Nc):
+            xbarc[j, i] = (1.0 / Ns) * np.sum(samples[j, :, i])  # (1)
+            sigma2c[j, i] = ((1.0 / (Ns - 1.0)) * np.sum((samples[j, :, i] -
+                             xbarc[j, i]) ** 2.0))  # (2)
+
+    # 3) Calculate the mean of all the chains for each parameter
+    # 4) Calculate the average of the individual chains' variances for each
+    #    parameter
+    # 5) Calculate the variance of the chains' means for each parameter
+    # 6) Calculate the ratio of each parameter
+    xbar = np.zeros(N)          # (3)
+    sigma2_chain = np.zeros(N)  # (4)
+    sigma2_mean = np.zeros(N)   # (5)
+    ratio = np.zeros(N)         # (6)
+    for i in range(N):
+        xbar[i] = (1.0 / Nc) * np.sum(xbarc[:, i])                         # (3)
+        sigma2_chain[i] = (1.0 / Nc) * np.sum(sigma2c[:, i])               # (4)
+        sigma2_mean[i] = (1.0 / Nc) * np.sum((xbarc[:,i] - xbar[i]) ** 2.0)# (5)
+        ratio[i] = (((((Nc - 1.0) / Nc) * sigma2_chain[i]) + ((1.0 / Nc) *
+                    sigma2_mean[i])) / sigma2_chain[i])                    # (6)
+
+    return ratio
