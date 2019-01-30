107bf3eaf68547af5131f4445db4a7f9126382f8
Sarah Gibson <sgibson@turing.ac.uk>
2019-01-30 18:53:10 +0000

    Add unit test for init_conds
    Add a unit test for init_conds func in magnetar library

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,4 +1,5 @@
 import numpy as np
+from magnetar.funcs import init_conds
 
 
 def geomean(list_a, list_b):
@@ -14,6 +15,9 @@ Function to calculate the geometric mean of pairs of numbers.
 
 
 def test_geomean():
+    """
+Test function for geomean function.
+    """
     list_a = [1.0, 2.0, 3.0]
     list_b = [4.0, 5.0, 6.0]
 
@@ -23,3 +27,18 @@ def test_geomean():
 
     assert geomean(list_a, list_b) == ans
 
+
+def test_init_conds():
+    """
+Test function for init_conds in magnetar library.
+    """
+    Msol = 1.99e33  # Solar mass in grams
+
+    MdiscI = 0.001  # Solar masses
+    P = 1.0         # milliseconds
+
+    Mdisc = MdiscI * Msol                 # Convert to grams
+    omega = (2.0 * np.pi) / (1.0e-3 * P)  # Convert to angular frequency
+
+    assert ((init_conds(MdiscI, P)[0] == Mdisc) &
+            (init_conds(MdiscI, P)[1] == omega))
