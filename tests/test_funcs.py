2797463bc2ef696b1daba18e8e7e87b60682b38a
Sarah Gibson <sgibson@turing.ac.uk>
2019-01-31 11:52:32 +0000

    Remove geometric mean test
    Custom function for geometric mean has been removed from kcorr.py
    and replaced with scipy's geometric mean function. Therefore, do
    not need to test the custom function.

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -3,32 +3,6 @@ from magnetar.funcs import init_conds
 from magnetar.fit_stats import redchisq
 
 
-def geomean(list_a, list_b):
-    """
-Function to calculate the geometric mean of pairs of numbers.
-
-    :param list_a: list or array of positive uncertainties
-    :param list_b: list or array of negative uncertainties
-    :return: list or array of the geometric mean of the pairs of numbers between
-             list_a and list_b
-    """
-    return [np.prod([i, np.abs(j)]) ** 0.5 for i, j in zip(list_a, list_b)]
-
-
-def test_geomean():
-    """
-Test function for geomean function.
-    """
-    list_a = [1.0, 2.0, 3.0]
-    list_b = [4.0, 5.0, 6.0]
-
-    ans = []
-    for x, y in zip(list_a, list_b):
-        ans.append((x * y) ** 0.5)
-
-    assert geomean(list_a, list_b) == ans
-
-
 def test_init_conds():
     """
 Test function for init_conds in magnetar library.
