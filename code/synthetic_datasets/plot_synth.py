c61d001da93308cca5c018ad550b1d75f47edfb8
Sarah Gibson <sgibson@turing.ac.uk>
2019-06-19 19:25:48 +0100

    Tidy up plot_synth.py

--- code/synthetic_datasets/plot_synth.py
+++ code/synthetic_datasets/plot_synth.py
@@ -4,31 +4,39 @@ import os
 import corner
 import argparse
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from sys import argv
 from matplotlib import rc
 from funcs import model_lum
-from fit_stats import AIC, redchisq
+from fit_stats import aicc, redchisq
 from matplotlib.ticker import MaxNLocator
+
+# Dictionary of true values for burst types
+truths = {
+    'Humped': [1.0, 5.0, -3.0, 2.0, -1.0, 0.0],
+    'Classic': [1.0, 5.0, -3.0, 3.0, -1.0, 0.0],
+    'Sloped': [1.0, 1.0, -3.0, 2.0, 1.0, 1.0],
+    'Stuttering': [1.0, 5.0, -5.0, 2.0, -1.0, 2.0]
+}
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description=""
+        description="Name of synthetic GRB and number of burn-in samples to parse"
     )
 
     parser.add_argument(
         "--grb",
         required=True,
         choices=["Humped", "Classic", "Sloped", "Stuttering"],
-        help=""
+        help="Name of synthetic GRB dataset"
     )
     parser.add_argument(
         "-b",
         "--n-burn",
         type=int,
         required=True,
-        help=""
+        help="Number of burn-in samples to remove"
     )
 
     return parser.parse_args()
@@ -38,6 +46,10 @@ def create_filenames(GRB):
     # Data basename and directory
     data_basename = os.path.join("data", "synthetic_datasets")
     data_dirname = os.path.join(data_basename, GRB)
+    if not os.path.exists(data_dirname):
+        raise FileNotFoundError(
+            "Please ensure your synthetic dataset and MCMC files have been generated."
+        )
 
     # Plot basename as directory
     plot_basename = os.path.join("plots", "synthetic_datasets")
@@ -59,47 +71,73 @@ def create_filenames(GRB):
     return fdata, fchain, fstats, fres, fplot_corner, fplot_model
 
 
-# Dictionary of true values for burst types
-truths = {
-    'Humped': [1.0, 5.0, -3.0, 2.0, -1.0, 0.0],
-    'Classic': [1.0, 5.0, -3.0, 3.0, -1.0, 0.0],
-    'Sloped': [1.0, 1.0, -3.0, 2.0, 1.0, 1.0],
-    'Stuttering': [1.0, 5.0, -5.0, 2.0, -1.0, 2.0]
-}
+def create_corner_plot(s, GRB, fplot):
+    # Latex strings for labels
+    names = [r'$B$', r'$P$', r'$\log_{10} (M_{\rm D,i})$',
+             r'$\log_{10} (R_{\rm D})$', r'$\log_{10} (\epsilon)$',
+             r'$\log_{10} (\delta)$']
 
-# Use LaTeX in plots
-rc('font',**{'family':'serif','serif':['Times']})
-rc('text', usetex=True)
+    fig = corner.corner(
+        s,
+        labels=names,
+        truths=np.array(truths[GRB]),
+        label_kwargs={'fontsize':12},
+        quantiles=[0.025, 0.5, 0.975],
+        plot_contours=True
+    )
+    fig.savefig("{0}".format(fplot))
+    plt.clf()
 
-# Arrays for upper and lower limits on parameters
-u = np.array([10.0, 10.0, -2.0, np.log10(2000.0), 2.0, 3.0])
-l = np.array([1.e-3, 0.69, -6.0, np.log10(50.0), -2.0, -1.0])
 
-# Get command line arguments
-args = parse_args()
+def create_model_plot(model, data, GRB, fplot):
+    fig = plt.figure(figsize=(6, 4.5))
+    ax = fig.add_subplot(111)
 
-# Create filenames
-fdata, fchain, fstats, fres, fplot_corner, fplot_model = \
-    create_filenames(args.grb)
+    ax.errorbar(data["x"], data["y"], yerr=data["yerr"], fmt='.r', capsize=0.0)
 
-# Read in data
-data = np.loadtxt(fdata, delimiter=",", skiprows=1)
-data = data[np.argsort(data[:,0])]
-x = data[:,0]
-y = data[:,1]
-yerr = data[:,2]
+    try:
+        ax.loglog(model[0, :], model[1, :], c='k')
+        ax.loglog(model[0, :], model[2, :], c='k', ls='--')
+        ax.loglog(model[0, :], model[3, :], c='k', ls=':')
+    except:
+        ax.set_xscale('log')
+        ax.set_yscale('log')
 
-# MCMC parameters
-Npars = 6      # Number of fitting parameters
-Nwalk = 50     # Number of walkers
-Nstep = 20000  # Number of MCMC steps
+    ax.set_xlim(1.0e0, 1.0e6)
+    ax.set_ylim(1.0e-8, 1.0e2)
+    ax.tick_params(axis='both', which='major', labelsize=10)
+    ax.set_xlabel(r'Time (s)', fontsize=12)
+    ax.set_ylabel(r'Luminosity $\left(10^{50}~{\rm erg}~{\rm s}^{-1}\right)$',
+                fontsize=12)
+    ax.set_title(r'{0}'.format(GRB), fontsize=12)
 
-# Read in chain & probability and reshape
-cols = tuple(range(Npars))
-skip = (args.n_burn * Nwalk) + 1
-s = np.loadtxt(fchain, delimiter=',', skiprows=1, usecols=cols)
+    fig.tight_layout()
+    fig.savefig("{0}".format(fplot))
+    plt.clf()
 
-body = """{0}
+
+def main():
+    # Get command line arguments
+    args = parse_args()
+
+    # Create filenames
+    fdata, fchain, fstats, fres, fplot_corner, fplot_model = \
+        create_filenames(args.grb)
+
+    # Read in data
+    data = pd.read_csv(fdata)
+
+    # MCMC parameters
+    Npars = 6      # Number of fitting parameters
+    Nwalk = 50     # Number of walkers
+    Nstep = 20000  # Number of MCMC steps
+
+    # Read in chain & probability and reshape
+    cols = tuple(range(Npars))
+    skip = (args.n_burn * Nwalk) + 1
+    samples = np.loadtxt(fchain, delimiter=',', skiprows=1, usecols=cols)
+
+    body = """{0}
 
 MC Parameters:
 Npars: {1}
@@ -108,44 +146,29 @@ Nstep: {3}
 Number of samples burned: {4}
 """.format(args.grb, Npars, Nwalk, Nstep, args.n_burn)
 
-#=== Plotting ===#
-# Latex strings for labels
-names = [r'$B$', r'$P$', r'$\log_{10} (M_{\rm D,i})$',
-         r'$\log_{10} (R_{\rm D})$', r'$\log_{10} (\epsilon)$',
-         r'$\log_{10} (\delta)$']
+    # Create corner plot
+    create_corner_plot(samples, args.grb, fplot_corner)
 
-# Corner plot
-fig = corner.corner(
-    s,
-    labels=names,
-    truths=np.array(truths[args.grb]),
-    label_kwargs={'fontsize':12},
-    quantiles=[0.025, 0.5, 0.975],
-    plot_contours=True
-)
-fig.savefig("{0}".format(fplot_corner))
-plt.clf()
+    # Correlations
+    corrs = []
+    for i in range(Npars):
+        j = i + 1
+        while j < Npars:
+            corrs.append(np.corrcoef(samples[:,i], samples[:,j])[0,1])
+            j += 1
 
-# Correlations
-corrs = []
-for i in range(Npars):
-    j = i + 1
-    while j < Npars:
-        corrs.append(np.corrcoef(s[:,i], s[:,j])[0,1])
-        j += 1
+    # Results
+    samples[:, 2:] = 10.0 ** samples[:, 2:]  # Convert out of log-space
 
-# Results
-s[:, 2:] = 10.0 ** s[:, 2:]  # Convert out of log-space
+    B, P, Md, Rd, eps, delt = map(
+        lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+        zip(*np.percentile(samples, [2.5, 50.0, 97.5], axis=0))
+    )
 
-B, P, Md, Rd, eps, delt = map(
-    lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
-    zip(*np.percentile(s, [2.5, 50.0, 97.5], axis=0))
-)
+    pars = [B[0], P[0], Md[0], Rd[0], eps[0], delt[0]]
 
-pars = [B[0], P[0], Md[0], Rd[0], eps[0], delt[0]]
-
-# Write results
-body += """\nResults:
+    # Write results
+    body += """\nResults:
            B (x10^15 G) = {0:.4f} (+{1:.4f}, -{2:.4f})\t(Truth: {3:.1f})
                  P (ms) = {4:.3f} (+{5:.3f}, -{6:.3f})\t(Truth: {7:.1f})
            Mdisc (Msol) = {8:.4} (+{9:.4}, -{10:.4})\t(Truth: {11:.6f})
@@ -158,44 +181,44 @@ body += """\nResults:
            eps[2], 10.0**truths[args.grb][4], delt[0], delt[1], delt[2],
            10.0**truths[args.grb][5])
 
-# Calculate model and fit statistics and write to file
-ymod = model_lum(pars, xdata=x)
-try:
-    # Calculate AIC
-    AIC = AIC(ymod, y, yerr, Npars)
-    body += "\nAIC = {0:.3f}".format(AIC)
-except:
-    chisq = redchisq(y, ymod, sd=yerr)
-    body += """\nChi Square = {0:.3f}
+    # Calculate model and fit statistics and write to file
+    ymod = model_lum(pars, xdata=data["x"])
+    try:
+        # Calculate AIC
+        AIC = aicc(ymod, data["y"], data["yerr"], Npars)
+        body += "\nAIC = {0:.3f}".format(AIC)
+    except:
+        chisq = redchisq(data["y"], ymod, sd=data["yerr"])
+        body += """\nChi Square = {0:.3f}
 N = {1}
-k = {2}""".format(chisq, len(y), Npars)
+k = {2}""".format(chisq, len(data["y"]), Npars)
 
-chisq_r = redchisq(y, ymod, deg=Npars, sd=yerr)
-body += "\nReduced Chi Square = {0:.3f}".format(chisq_r)
+    chisq_r = redchisq(data["y"], data["ymod"], deg=Npars, sd=data["yerr"])
+    body += "\nReduced Chi Square = {0:.3f}".format(chisq_r)
 
-body += ('\n\n{0}'.format(args.grb) + ' & $' +
-         '{0}'.format(B[0]) + '^{+' +
-         '{0}'.format(B[1]) + '}_{-' +
-         '{0}'.format(B[2]) + '}$ & $' +
-         '{0}'.format(P[0]) + '^{+' +
-         '{0}'.format(P[1]) + '}_{-' +
-         '{0}'.format(P[2]) + '}$ & $' +
-         '{0}'.format(Md[0]) + '^{+' +
-         '{0}'.format(Md[1]) + '}_{-' +
-         '{0}'.format(Md[2]) + '}$ & $' +
-         '{0}'.format(Rd[0]) + '^{+' +
-         '{0}'.format(Rd[1]) + '}_{-' +
-         '{0}'.format(Rd[2]) + '}$ & $' +
-         '{0}'.format(eps[0]) + '^{+' +
-         '{0}'.format(eps[1]) + '}_{-' +
-         '{0}'.format(eps[2]) + '}$ & $' +
-         '{0}'.format(delt[0]) + '^{+' +
-         '{0}'.format(delt[1]) + '}_{-' +
-         '{0}'.format(delt[2]) + '}$ & $' +
-         '{0}'.format(chisq_r) + '$ \\\\ [2pt]')
+    body += ('\n\n{0}'.format(args.grb) + ' & $' +
+             '{0}'.format(B[0]) + '^{+' +
+             '{0}'.format(B[1]) + '}_{-' +
+             '{0}'.format(B[2]) + '}$ & $' +
+             '{0}'.format(P[0]) + '^{+' +
+             '{0}'.format(P[1]) + '}_{-' +
+             '{0}'.format(P[2]) + '}$ & $' +
+             '{0}'.format(Md[0]) + '^{+' +
+             '{0}'.format(Md[1]) + '}_{-' +
+             '{0}'.format(Md[2]) + '}$ & $' +
+             '{0}'.format(Rd[0]) + '^{+' +
+             '{0}'.format(Rd[1]) + '}_{-' +
+             '{0}'.format(Rd[2]) + '}$ & $' +
+             '{0}'.format(eps[0]) + '^{+' +
+             '{0}'.format(eps[1]) + '}_{-' +
+             '{0}'.format(eps[2]) + '}$ & $' +
+             '{0}'.format(delt[0]) + '^{+' +
+             '{0}'.format(delt[1]) + '}_{-' +
+             '{0}'.format(delt[2]) + '}$ & $' +
+             '{0}'.format(chisq_r) + '$ \\\\ [2pt]')
 
-try:
-    body += """\n\nCorrelations:
+    try:
+        body += """\n\nCorrelations:
              B->P: {0:.3f}
      B->log10(Md): {1:.3f}
      B->log10(Rd): {2:.3f}
@@ -218,45 +241,29 @@ log10(epsilon)->log10(delta): {14:.3f}
 """.format(corrs[0], corrs[1], corrs[2], corrs[3], corrs[4], corrs[5],
            corrs[6], corrs[7], corrs[8], corrs[9], corrs[10], corrs[11],
            corrs[12], corrs[13], corrs[14])
-except:
-    pass
+    except:
+        pass
 
-# Smoothed model
-fit = model_lum(pars)
+    # Smoothed model
+    fit = model_lum(pars)
 
-# Write to a file
-with open(fstats, 'w') as f:
-    f.write(body)
-print("Results written to: {0}".format(fstats))
+    # Write to a file
+    with open(fstats, 'w') as f:
+        f.write(body)
+    print("Results written to: {0}".format(fstats))
 
-# Plot the smoothed model
-fig = plt.figure(figsize=(6, 4.5))
-ax = fig.add_subplot(111)
+    # Plot the smoothed model
+    create_filenames(fit, data, args.grb, fplot_model)
 
-ax.errorbar(x, y, yerr=yerr, fmt='.r', capsize=0.0)
+    # Write smoothed model to a file
+    model = pd.DataFrame({
+        "t": fit[0, :],
+        "Ltot": fit[1, :],
+        "Lprop": fit[2, :],
+        "Ldip": fit[3, :]
+    })
+    model.to_csv(fres)
 
-try:
-    ax.loglog(fit[0,:], fit[1,:], c='k')
-    ax.loglog(fit[0,:], fit[2,:], c='k', ls='--')
-    ax.loglog(fit[0,:], fit[3,:], c='k', ls=':')
-except:
-    ax.set_xscale('log')
-    ax.set_yscale('log')
 
-ax.set_xlim(1.0e0, 1.0e6)
-ax.set_ylim(1.0e-8, 1.0e2)
-ax.tick_params(axis='both', which='major', labelsize=10)
-ax.set_xlabel(r'Time (s)', fontsize=12)
-ax.set_ylabel(r'Luminosity $\left(10^{50}~{\rm erg}~{\rm s}^{-1}\right)$',
-              fontsize=12)
-ax.set_title(r'{0}'.format(args.grb), fontsize=12)
-
-fig.tight_layout()
-fig.savefig("{0}".format(fplot_model))
-plt.clf()
-
-# Write smoothed model to a file
-with open(fres, 'w') as f:
-    for i in range(fit.shape[1]):
-        f.write('{0}, {1}, {2}, {3}\n'.format(fit[0,i], fit[1,i], fit[2,i],
-                fit[3,i]))
+if __name__ == "__main__":
+    main()
